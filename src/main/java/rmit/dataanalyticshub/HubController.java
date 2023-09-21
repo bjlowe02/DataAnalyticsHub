a7a2cda11d4bfd96b2f97a96fc7c28a3f592f2f4
Brodie Lowe <bjlowe@outlook.com>
2023-09-21 19:52:23 +1000

    created main form and controller
    on successful login, login form will close and open main

---
+++ src/main/java/rmit/dataanalyticshub/HubController.java
@@ -0,0 +1,5 @@
+package rmit.dataanalyticshub;
+
+
+public class HubController {
+}
--- src/main/java/rmit/dataanalyticshub/LoginController.java
+++ src/main/java/rmit/dataanalyticshub/LoginController.java
@@ -2,15 +2,18 @@ package rmit.dataanalyticshub;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
 
 import javax.swing.*;
-import java.net.URL;
+import java.io.IOException;
 import java.sql.SQLException;
-import java.util.ResourceBundle;
 
 public class LoginController {
     public LoginModel loginModel = new LoginModel();
@@ -37,8 +40,20 @@ public class LoginController {
         } else { //good input will continue to check against db
             try {
                 if (loginModel.isLogin(txtID.getText(), txtPassword.getText())){
-                    //TODO create and open main form
-                    System.out.println("Success!");
+                    //TODO store user details
+
+                    //close current form
+                    ((Node)event.getSource()).getScene().getWindow().hide();
+                    //open next from with logged-in user
+                    FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("Hub.fxml"));
+                    Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+                    Stage stage = new Stage();
+                    stage.getIcons().add(new Image("file:src/icon.png"));
+                    stage.setTitle("Data Analytics Hub | Login");
+                    stage.setScene(scene);
+                    stage.show();
+
+
                 } else {
                     //warning message
                     JOptionPane.showMessageDialog(null,
@@ -51,6 +66,8 @@ public class LoginController {
                 }
             } catch (SQLException e) {
                 System.out.println(e);
+            } catch (IOException e) {
+                //System.out.println(e);
             }
         }
     }
