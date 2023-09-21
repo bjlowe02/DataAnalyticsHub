7c658d9e1ccb72bbf95c3451c150efceeb0fc8aa
Brodie Lowe <bjlowe@outlook.com>
2023-09-21 14:28:43 +1000

    created database and connection
    added sqlite db and connector class

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module rmit.dataanalyticshub {
     requires javafx.fxml;
 
     requires com.dlsc.formsfx;
+    requires java.sql;
 
     opens rmit.dataanalyticshub to javafx.fxml;
     exports rmit.dataanalyticshub;
---
+++ src/main/java/rmit/dataanalyticshub/SqliteConnection.java
@@ -0,0 +1,13 @@
+package rmit.dataanalyticshub;
+import java.sql.*;
+public class SqliteConnection {
+    public static Connection Connector() {
+        try {
+            Class.forName("org.sqlite.JDBC");
+            Connection conn = DriverManager.getConnection("jdbc:sqlite:db.sqlite");
+            return conn;
+        } catch (Exception e){
+            return null;
+        }
+    }
+}
