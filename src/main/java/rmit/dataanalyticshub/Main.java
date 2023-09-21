24477a2b854842030aad833382eb97d735a52839
Brodie Lowe <bjlowe@outlook.com>
2023-09-21 18:18:26 +1000

    misc

--- src/main/java/rmit/dataanalyticshub/Main.java
+++ src/main/java/rmit/dataanalyticshub/Main.java
@@ -14,7 +14,7 @@ public class Main extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("Login.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.getIcons().add(new Image("file:src/icon.png"));
-        stage.setTitle("Data Analytics Hub | Login!");
+        stage.setTitle("Data Analytics Hub | Login");
         stage.setScene(scene);
         stage.show();
     }
