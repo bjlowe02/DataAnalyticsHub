6a26a811969aec1cb6347446b3f1a009d119ad85
Brodie Lowe <bjlowe@outlook.com>
2023-09-21 14:07:12 +1000

    added icon

--- src/main/java/rmit/dataanalyticshub/HelloApplication.java
+++ src/main/java/rmit/dataanalyticshub/HelloApplication.java
@@ -3,6 +3,7 @@ package rmit.dataanalyticshub;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -12,7 +13,8 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.getIcons().add(new Image("file:src/icon.png"));
+        stage.setTitle("Data Analytics Hub | Login!");
         stage.setScene(scene);
         stage.show();
     }
