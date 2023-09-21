b90887ac6e212479cd38f53fb4a7462853684e3c
Brodie Lowe <bjlowe@outlook.com>
2023-09-21 14:08:54 +1000

    created MVC login files
    added login model, view, and controller

--- src/main/java/rmit/dataanalyticshub/LoginController.java
+++ src/main/java/rmit/dataanalyticshub/LoginController.java
@@ -3,7 +3,7 @@ package rmit.dataanalyticshub;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class LoginController {
     @FXML
     private Label welcomeText;
 
--- src/main/java/rmit/dataanalyticshub/Main.java
+++ src/main/java/rmit/dataanalyticshub/Main.java
@@ -8,10 +8,10 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("Login.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.getIcons().add(new Image("file:src/icon.png"));
         stage.setTitle("Data Analytics Hub | Login!");
