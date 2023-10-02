04091e556d39b08ca932b463221d3dd175e9a3e3
Brodie Lowe <bjlowe@outlook.com>
2023-10-02 19:10:18 +1100

    added method to retrieve user details
    details will be displayed in GUI and can further be edited

--- src/main/java/rmit/dataanalyticshub/HubController.java
+++ src/main/java/rmit/dataanalyticshub/HubController.java
@@ -1,27 +1,59 @@
 package rmit.dataanalyticshub;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
 
-import java.sql.SQLException;
-
 public class HubController {
     public HubModel hubModel = new HubModel();
+    private User currentUser;
 
+    //FXML variables
     @FXML
     private Label lblWelcome;
     @FXML
     private Pane paneProfile;
     @FXML
     private Pane panePost;
+    @FXML
+    private Pane paneWelcome;
+    @FXML
+    private Button btnProfile;
+    @FXML
+    private Button btnPost;
+    @FXML
+    private Label lblFName;
+    @FXML
+    private Label lblLName;
+    @FXML
+    private Label lblID;
+    @FXML
+    private Label lblVIP;
 
     public void setUser(User user){
+        //sets current user
+        this.currentUser = user;
+        //Prepares window and greets user with welcome message
         //Hide panes
         paneProfile.setVisible(false);
         panePost.setVisible(false);
         //display current user
         lblWelcome.setText("Welcome back, " +
                 user.getFirstName() + "!");
+    }
+
+    @FXML
+    protected void onBtnProfileClick(javafx.scene.input.MouseEvent mouseEvent) {
+        //puts stores user details into GUI
+/*        lblFName.setText(currentUser.getFirstName());
+        lblLName.setText(currentUser.getLastName());
+        lblID.setText(String.valueOf(currentUser.getID()));
+        lblVIP.setText(String.valueOf(currentUser.isVIP()));*/
+        //hide other panes
+        paneWelcome.setVisible(false);
+        panePost.setVisible(false);
+        //show profile pane
+        paneProfile.setVisible(true);
     }
 }
--- src/main/java/rmit/dataanalyticshub/User.java
+++ src/main/java/rmit/dataanalyticshub/User.java
@@ -4,13 +4,27 @@ public class User {
     private int ID;
     private String firstName;
     private String lastName;
+    private boolean VIP;
 
     public User(int ID, String firstName, String lastName) {
         this.ID = ID;
         this.firstName = firstName;
         this.lastName = lastName;
+        this.VIP = false;
     }
     public String getFirstName() {
         return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public boolean isVIP() {
+        return VIP;
     }
 }
