881f96c0b60f6c0a2383e6fc15c2cfcf68a1cc31
Brodie Lowe <bjlowe@outlook.com>
2023-10-16 11:53:52 +1100

    rearranged files into packages
    will require further debugging to resolve issues regrading new file locations

--- src/main/java/rmit/dataanalyticshub/Main.java
+++ src/main/java/rmit/dataanalyticshub/Main.java
@@ -1,7 +1,6 @@
 package rmit.dataanalyticshub;
 
 import javafx.application.Application;
-import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
@@ -12,9 +11,9 @@ import java.io.IOException;
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/login/Login.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("views/Login.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.getIcons().add(new Image("file:src/icon.png"));
+        stage.getIcons().add(new Image("file:src/main/resources/rmit/dataanalyticshub/images/icon.png"));
         stage.setTitle("Data Analytics Hub | Login");
         stage.setScene(scene);
         stage.show();
@@ -28,4 +27,4 @@ public class Main extends Application {
     public static void main(String[] args) {
         launch();
     }
-}
+} 
